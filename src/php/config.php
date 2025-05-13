2223556819985e55346d765c49bf1195c1ad2d9b
JhoendryB <jhoendryblanco@htmail.com>
2025-05-13 14:26:46 -0400

    add db config basic for xample

---
+++ src/php/config.php
@@ -0,0 +1,7 @@
+<?php
+// Configuracion de la base de datos
+define('HOST_MYSQL', 'localhost');
+define('USER_MYSQL', 'root');
+define('PASS_MYSQL', 'usbw');
+define('DB_MYSQL', 'administrador');
+define('CHATSET_MYSQL', 'utf8');
