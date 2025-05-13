2cbc57d3012b6a3dfcf7bdfd38b07348448d108a
JhoendryB <jhoendryblanco@htmail.com>
2025-05-13 14:29:00 -0400

    config table xample dinamic data

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,31 @@
 $(function () {
-    $('#example').DataTable();
+    tablaOne = tablaDinamica({
+        titulo: "Ciudades",
+        elemento: "#example",
+        caracter: true,
+        ...(JSON.parse(atob(datatableDinamico))),
+    }, {
+        columns: [
+            {
+                "data": "ciudad",
+            },
+            {
+                "data": "estado",
+            },
+            {
+                "data": function (row, data) {
+                    let btn;
+                    if (row) {
+                        const dataAtrib = "prueba";
+                        btn = ` 
+                                <div class="btn-group btn-group-sm">
+                                    <a href="#" class="btn btn-info d-flex justify-content-center align-items-center" onclick="eventos(this)" ${dataAtrib}><i class="fa fa-eye"></i></a>
+                                </div>
+                            `;
+                    }
+                    return btn;
+                },
+            }
+        ],
+    });
 });
